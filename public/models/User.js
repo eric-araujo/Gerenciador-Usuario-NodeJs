3b1a01bbf90e2eea94c9ac70bc867010df965be1
eric-araujo <eric.araujolima@hotmail.com>
2020-07-01 16:40:08 -0300

    Utilizando Fetch API

--- public/models/User.js
+++ public/models/User.js
@@ -101,7 +101,7 @@ class User {
 
     static getUsersStorage() {
 
-        return HttpRequest.get('/users');
+        return Fetch.get('/users');
 
     }
 
@@ -127,11 +127,11 @@ class User {
 
             if (this.id) {
 
-                promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+                promise = Fetch.put(`/users/${this.id}`, this.toJSON());
 
             } else {
 
-                promise = HttpRequest.post(`/users`, this.toJSON());
+                promise = Fetch.post(`/users`, this.toJSON());
 
             }
 
@@ -153,7 +153,7 @@ class User {
 
     remove() {
 
-        return HttpRequest.delete(`/users/${this.id}`);
+        return Fetch.delete(`/users/${this.id}`);
 
     }
 
